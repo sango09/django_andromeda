cfff302a44fa929296d06b3b7404f6cf22dac5e7
Santiago Gomez Florez <santiagofl123@gmail.com>
2020-08-27 18:06:49 -0500

    Archivos django

--- django_andromeda/settings.py
+++ django_andromeda/settings.py
@@ -49,7 +49,7 @@ MIDDLEWARE = [
     'django.middleware.clickjacking.XFrameOptionsMiddleware',
 ]
 
-ROOT_URLCONF = 'djangoProject1.urls'
+ROOT_URLCONF = 'django_andromeda.urls'
 
 TEMPLATES = [
     {
